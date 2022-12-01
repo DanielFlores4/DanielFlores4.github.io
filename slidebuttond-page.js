2d4302f424b34508cd74fa9ce69af34ef92d5c27
DanielFlores4 <daniel.flores204306@potros.itson.edu.mx>
2022-12-01 00:55:52 -0700

    Added beta 1

---
+++ slidebuttond-page.js
@@ -0,0 +1,10 @@
+let click = document.querySelector('.click');
+        
+                    let list = document.querySelector('.list-page');
+        
+                    click.addEventListener("click",()=>{
+        
+                        list.classList.toggle('newlist');
+        
+                    });
+        
